6568790fe78da16be4dc42c1f1a2c238e6f783e8
Lena Pavlushko <lena@lenas-mbp.mynet>
2017-08-20 17:04:44 +0300

    gitignore fixed

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // TWITTER APP CONFIGURATION
 
 module.exports = {
-    consumer_key: 'bfJZUfDJc5H84ocagVInOkiLK',  
-    consumer_secret: 's5W9mmu0DxP7RLuZj4eXIufX5Lo7rfgv0pFSiopJEx7wF7kgup',
-    access_token: '596290922-rXNX2xMmqM86tLP02DI30PfCv9jzmme3TFFtL9BY',  
-    access_token_secret: 'yja5KwnZYP78zZAgUkRrRbWxyflLEyEnZ4rdLr02ikkCm'
+    consumer_key: '',  
+    consumer_secret: '',
+    access_token: '',  
+    access_token_secret: ''
   }
